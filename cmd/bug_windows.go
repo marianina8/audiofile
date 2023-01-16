b2928c7c003532f6646fe3f3cc339cf20112ef04
Marian Montagnino <mmontagnino@netflix.com>
2023-01-15 22:48:04 -0800

    fix bug command for windows

--- cmd/bug_windows.go
+++ cmd/bug_windows.go
@@ -7,5 +7,5 @@ import (
 )
 
 func openBrowser(targetURL string) bool {
-	exec.Command("rundll32.exe", "url.dll,FileProtocolHandler", targetURL).Start() == nil
+	return exec.Command("cmd", "/C", "start", "msedge", targetURL).Start() == nil
 }
