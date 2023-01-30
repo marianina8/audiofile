496a2d28524f7fd0269dcec33690d2fc328dca26
Marian Montagnino <mmontagnino@netflix.com>
2023-01-29 20:43:34 -0800

    fix play functions

--- cmd/play_darwin.go
+++ cmd/play_darwin.go
@@ -9,25 +9,21 @@ import (
 	"github.com/pterm/pterm"
 )
 
-func play(audiofilePath string, verbose, disableOutput bool) (int, error) {
-	cmd := exec.Command("afplay", audiofilePath)
-	if err := cmd.Start(); err != nil {
-		return 0, utils.Error("\n  starting afplay command: %v", err, verbose)
-	}
-	if !disableOutput {
-		spinnerInfo := &pterm.SpinnerPrinter{}
-		if utils.IsaTTY() {
-			spinnerInfo, _ = pterm.DefaultSpinner.Start("Enjoy the music...")
-		}
-		err := cmd.Wait()
-		if err != nil {
-			return 0, utils.Error("\n  running afplay command: %v", err, verbose)
-		}
-		if utils.IsaTTY() {
-			spinnerInfo.Stop()
-		}
-		return 0, nil
-	} else {
-		return cmd.Process.Pid, nil
-	}
+func play(audiofilePath string, verbose bool) (int, error) {
+    cmd := exec.Command("afplay", audiofilePath)
+    if err := cmd.Start(); err != nil {
+        return 0, utils.Error("\n  starting afplay command: %v", err, verbose)
+    }
+    spinnerInfo := &pterm.SpinnerPrinter{}
+    if utils.IsaTTY() {
+        spinnerInfo, _ = pterm.DefaultSpinner.Start("Enjoy the music...")
+    }
+    err := cmd.Wait()
+    if err != nil {
+        return 0, utils.Error("\n  running afplay command: %v", err, verbose)
+    }
+    if utils.IsaTTY() {
+        spinnerInfo.Stop()
+    }
+    return cmd.Process.Pid, nil
 }
